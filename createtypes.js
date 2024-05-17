199344b1f405c91cba604e0d149d97bbaeab42fc
ant-7802 <anttubemc@googlemail.com>
2024-05-17 18:40:19 -0400

    Here

--- createtypes.js
+++ createtypes.js
@@ -1,5 +1,7 @@
 const fs = require("fs")
 var pong = fs.readFileSync("pong.js",'utf-8')
+var readme = fs.readFileSync(".tREADME.md",'utf-8')
 var bookmarklet = "javascript:" +  encodeURIComponent("(function(){" + pong.trim() +  "})();");
 fs.writeFileSync("bookmarklet.js",bookmarklet)
 fs.writeFileSync("pong.html",`<!DOCTYPE html><html><body><style>html { background-color: black; } </style> <script>window.pheight = true; ${pong}</script></body></html>`)
+fs.writeFileSync("README.md",readme.replace("PONGEMBED",`<a href='${bookmarklet}'>Drag me to your bookmarks</a>`))
